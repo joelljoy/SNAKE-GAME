e06e446ffb128b661e9de02d919c524bec836bef
Joel Joy <joeljoy8888@gmail.com>
2026-03-05 01:55:27 +0530

    Update snake_game.c

--- snake_game.c
+++ snake_game.c
@@ -59,7 +59,7 @@ Position food_pos(snake_node* head) {
 void gameBox(snake_node* head, Position food) { //using ncurses function
 	//top and bottom
 	for(int i=0; i<width; i++) {
-		mvaddch(y_off,x_off+i,bound);//y,x,char - top
+		mvaddch(y_off,x_off+i,bound);
 		mvaddch(y_off+height-1,x_off+i,bound);//bottom
 	}
 	//left and right
